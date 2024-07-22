8ef5ec36850b0582fe1f828ed6372dbcabc8247b
kamranshez <kamranshez@gmail.com>
2024-07-23 01:54:34 +0500

    c2

--- app/main.py
+++ app/main.py
@@ -5,5 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World1", "World2": "Pakistan zinda bad 1234567 80"}
+    return {"Hello": "World1", "World2": "This is pakistan"}
+
+@app.get("about/")
+def about_page():
+    return {"Institute": "World1", "World2": "Pakistan zinda bad 1234567 80"}
 
